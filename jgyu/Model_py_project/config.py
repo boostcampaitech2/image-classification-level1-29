c7bd0b80670611194d534b37c284ab617ba34033
JeangyuHeo <gjwlsrb1700@naver.com>
2021-08-25 13:29:24 +0000

    fix : config structure

--- jgyu/Model_py_project/config.py
+++ jgyu/Model_py_project/config.py
@@ -16,23 +16,29 @@ import torch.nn.functional as F
 from tqdm.auto import tqdm
 from sklearn.metrics import f1_score
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import Resize, ToTensor, Normalize, CenterCrop
+from torchvision.transforms import Resize, ToTensor, Normalize, CenterCrop, RandomHorizontalFlip, RandomRotation
+from torchvision.models import resnext50_32x4d
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-class cfg:
-    data_dir = "/opt/ml/input/data/train/"
-    img_dir = f"{data_dir}/images"
-    df_path = f"{data_dir}/train.csv"
-    img_height = 512
-    img_width = 384
-    batch_size = 128
-    lr = 0.001
-    epoch = 10
+device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
 
-    mask_labels=[]
-    age_labels=[]
-    gender_labels=[]
-    imgs=[]
-    ans = []
-    df = pd.read_csv(df_path)
+CLASS_NUM=18
+
+data_dir = "/opt/ml/input/data/train/"
+img_dir = f"{data_dir}/images"
+df_path = f"{data_dir}/train.csv"
+
+img_height = 512
+img_width = 384
+batch_size = 128
+lr = 0.001
+epoch = 10
+
+mask_labels=[]
+age_labels=[]
+gender_labels=[]
+imgs=[]
+ans = []
+    
+df = pd.read_csv(df_path)
