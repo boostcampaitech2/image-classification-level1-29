6ce42ec8bf1e68bf6e8059ce3ed0674b84143c66
barleysack <hookypooky@naver.com>
2021-08-30 13:04:47 +0000

    epoch setting added

--- team/29.sh
+++ team/29.sh
@@ -14,27 +14,31 @@ echo "	    5. Inception-ResnetV2		            "
 echo "=============================================="
 
 read modelnumber
-
+echo "Please enter epoch number, if you don't, it will be 10."
+read epochnumber
+if [ -z "$epochnumber" ]; then
+    epochnumber=10
+fi 
 
 case $modelnumber in
     1)
-        python3 train.py --model EfficientNet_b3
+        python3 train.py --model EfficientNet_b3 --epoch $epochnumber
     ;;
 
 	2) 
-        python3 train.py --model Res2Next50
+        python3 train.py --model Res2Next50 --epoch $epochnumber
     ;;
 
     3) 
-        python3 train.py --model ResNext50
+        python3 train.py --model ResNext50 --epoch $epochnumber
     ;;
 
     4) 
-        python3 train.py --model DenseNet121
+        python3 train.py --model DenseNet121 --epoch $epochnumber
     ;;
 
     5)
-        python3 train.py --model InceptionResnetv2
+        python3 train.py --model InceptionResnetv2 --epoch $epochnumber
     ;;
     
 esac
