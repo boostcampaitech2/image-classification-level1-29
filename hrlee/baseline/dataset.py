5d764d1bdabe8a2575666c0b893cfd7cefd37ddd
hrxorxm <twinha0204@gmail.com>
2021-09-01 16:09:53 +0000

    add FaceCrop dataset using facenet & retinaface

--- hrlee/baseline/dataset.py
+++ hrlee/baseline/dataset.py
@@ -14,6 +14,7 @@ from pandas_streaming.df import train_test_apart_stratify
 from torchvision import transforms
 from torchvision.transforms import *
 from facenet_pytorch import MTCNN
+from retinaface import RetinaFace
 from auto_augment import AutoAugment, Cutout
 
 
@@ -391,3 +392,169 @@ class TestDataset(Dataset):
 
     def set_transform(self, transform):
         self.transform = transform
+
+
+class FaceDataset(Dataset):
+    num_classes = 3 * 2 * 3
+
+    _file_names = {
+        "mask1": MaskLabels.MASK,
+        "mask2": MaskLabels.MASK,
+        "mask3": MaskLabels.MASK,
+        "mask4": MaskLabels.MASK,
+        "mask5": MaskLabels.MASK,
+        "incorrect_mask": MaskLabels.INCORRECT,
+        "normal": MaskLabels.NORMAL
+    }
+
+    image_paths = []
+    mask_labels = []
+    gender_labels = []
+    age_labels = []
+    all_labels = []
+    indexs = []
+    groups = []
+
+    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
+        self.data_dir = data_dir
+        self.mean = mean
+        self.std = std
+        self.val_ratio = val_ratio
+
+        self.transform = None
+        self.setup()
+        self.calc_statistics()
+
+    def setup(self):
+        cnt = 0
+        profiles = os.listdir(self.data_dir)
+        for profile in profiles:
+            if profile.startswith("."):  # "." 로 시작하는 파일은 무시합니다
+                continue
+
+            img_folder = os.path.join(self.data_dir, profile)
+            for file_name in os.listdir(img_folder):
+                _file_name, ext = os.path.splitext(file_name)
+                if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                    continue
+
+                img_path = os.path.join(self.data_dir, profile, file_name)  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                mask_label = self._file_names[_file_name]
+
+                id, gender, race, age = profile.split("_")
+                gender_label = GenderLabels.from_str(gender)
+                age_label = AgeLabels.from_number(age)
+
+                self.image_paths.append(img_path)
+                self.mask_labels.append(mask_label)
+                self.gender_labels.append(gender_label)
+                self.age_labels.append(age_label)
+                self.all_labels.append(self.encode_multi_class(mask_label, gender_label, age_label))
+                self.indexs.append(cnt)
+                self.groups.append(id)
+                cnt += 1
+
+    def calc_statistics(self):
+        has_statistics = self.mean is not None and self.std is not None
+        if not has_statistics:
+            print("[Warning] Calculating statistics... It can take a long time depending on your CPU machine")
+            sums = []
+            squared = []
+            for image_path in self.image_paths[:3000]:
+                image = np.array(Image.open(image_path)).astype(np.int32)
+                sums.append(image.mean(axis=(0, 1)))
+                squared.append((image ** 2).mean(axis=(0, 1)))
+
+            self.mean = np.mean(sums, axis=0) / 255
+            self.std = (np.mean(squared, axis=0) - self.mean ** 2) ** 0.5 / 255
+
+    def set_transform(self, transform):
+        self.transform = transform
+
+    def __getitem__(self, index):
+        assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
+
+        image = self.read_image(index)
+        mask_label = self.get_mask_label(index)
+        gender_label = self.get_gender_label(index)
+        age_label = self.get_age_label(index)
+        multi_class_label = self.encode_multi_class(mask_label, gender_label, age_label)
+        
+        # ===== Face Crop =====
+        # padding value before cropping
+        X_PADDING = 20
+        Y_PADDING = 30
+        # mtcnn 적용
+        mtcnn = MTCNN(keep_all=True)
+        boxes, probs = mtcnn.detect(image)
+        if len(probs) > 1:
+            xmin = int(boxes[0, 0]) - X_PADDING
+            ymin = int(boxes[0, 1]) - Y_PADDING
+            xmax = int(boxes[0, 2]) + X_PADDING
+            ymax = int(boxes[0, 3]) + Y_PADDING
+        else:
+            # retinaface 적용
+            image_path = self.image_paths[index]
+            result_detected = RetinaFace.detect_faces(image_path)
+            if type(result_detected) == dict:
+                xmin = int(result_detected["face_1"]["facial_area"][0]) - X_PADDING
+                ymin = int(result_detected["face_1"]["facial_area"][1]) - Y_PADDING
+                xmax = int(result_detected["face_1"]["facial_area"][2]) + X_PADDING
+                ymax = int(result_detected["face_1"]["facial_area"][3]) + Y_PADDING
+            else:
+                # 직접 crop
+                xmin = 80
+                ymin = 50
+                xmax = 80 + 220
+                ymax = 50 + 320
+        if xmin < 0: xmin = 0
+        if ymin < 0: ymin = 0
+        if xmax > 384: xmax = 384
+        if ymax > 512: ymax = 512
+        image = image.crop([xmin, ymin, xmax, ymax])
+        # transform
+        image_transform = self.transform(image)
+        return image_transform, multi_class_label
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def get_mask_label(self, index) -> MaskLabels:
+        return self.mask_labels[index]
+
+    def get_gender_label(self, index) -> GenderLabels:
+        return self.gender_labels[index]
+
+    def get_age_label(self, index) -> AgeLabels:
+        return self.age_labels[index]
+
+    def read_image(self, index):
+        image_path = self.image_paths[index]
+        return Image.open(image_path)
+
+    @staticmethod
+    def encode_multi_class(mask_label, gender_label, age_label) -> int:
+        return mask_label * 6 + gender_label * 3 + age_label
+
+    @staticmethod
+    def decode_multi_class(multi_class_label) -> Tuple[MaskLabels, GenderLabels, AgeLabels]:
+        mask_label = (multi_class_label // 6) % 3
+        gender_label = (multi_class_label // 3) % 2
+        age_label = multi_class_label % 3
+        return mask_label, gender_label, age_label
+
+    @staticmethod
+    def denormalize_image(image, mean, std):
+        img_cp = image.copy()
+        img_cp *= std
+        img_cp += mean
+        img_cp *= 255.0
+        img_cp = np.clip(img_cp, 0, 255).astype(np.uint8)
+        return img_cp
+
+    def split_dataset(self) -> Tuple[Subset, Subset]:
+        df = pd.DataFrame({"indexs":self.indexs, "groups":self.groups, "labels":self.all_labels})
+        train, valid = train_test_apart_stratify(df, group="groups", stratify="labels", test_size=self.val_ratio)
+        train_index = train["indexs"].tolist()
+        valid_index = valid["indexs"].tolist()
+        return  [Subset(self, train_index), Subset(self, valid_index)]
